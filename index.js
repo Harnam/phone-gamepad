1906d79a2ff4ea1818e3cdbb9d50d745c58446a3
Harnam Singh <harnamsinghsandhu04@gmail.com>
2020-11-23 12:57:12 +0530

    made simple electron app

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+const { app , BrowserWindow } = require('electron');
 var express = require('express');
 
-var app = express();
+var exp = express();
 
 var port = process.env.port || 15987;
 
-app.get('/', (req, res) => {
-    res.send("hello world!");
-});
+exp.use(express.static(__dirname + '/public'));
 
-app.listen(port, (err) => {
+exp.listen(port, (err) => {
     if (err) throw err;
     console.log("App running on port: " + port);
+});
+
+createwindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    win.loadURL('http://localhost:' + port);
+    win.focus();
+    win.webContents.openDevTools();
+};
+
+app.whenReady().then(createwindow);
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
+
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0)
+        createwindow();
 });
