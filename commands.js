9cfdc878f93dfef8fc959611edfe5ef427c37c3d
Harnam Singh <harnamsinghsandhu04@gmail.com>
2020-11-25 17:05:30 +0530

    added start stop java program

--- commands.js
+++ commands.js
@@ -3,6 +3,11 @@ var express = require('express');
 var exp = express();
 exp.use(require('body-parser').json());
 
+var { spawn } = require('child_process');
+const { Stream } = require('stream');
+
+var java;
+
 var port = 15987;
 
 var isStarted = false;
@@ -13,12 +18,15 @@ startserver = () => {
     server = exp.listen(port, (err) => {
         if (err) throw err;
         console.log("App running on port: " + port);
+        java = spawn("java", [ "-cp", "jrobot", "robot" ]);
+        startJava();
     });
     isStarted = true;
 };
 
 stopserver = () => {
     server.close();
+    java.kill('SIGINT');
     isStarted = false;
     console.log("App closed")
 }
@@ -36,3 +44,19 @@ module.exports = {
     stopserver,
     changeport
 };
+
+startJava = () => {
+    java.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+    
+    java.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+    
+    java.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+    //process.stdin.pipe(java.stdin);
+    java.stdin.write("hello there general kenobi\n");
+}
---
+++ jrobot/robot.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+import java.util.*;
+
+public class robot {
+    public static void main(String args[]) throws IOException, AWTException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        Robot robot = new Robot();
+        System.out.println("Starting java file");
+        while (true) {
+            String s = sc.nextLine();
+            System.out.println(s);
+        }
+    }
+}
